17446357b8d48be220bcc32e0458ca8cf9524dde
Danil Shapilov <danil@shapilov.dev>
2025-06-04 18:13:24 +0500

    connect db, add register user command

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/DanilShapilov/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -10,10 +15,46 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	username := cmd.Args[0]
 
-	err := s.cfg.SetUser(username)
+	user, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
-	fmt.Printf("User was set to: %s\n", username)
+	fmt.Printf("User successfully switched to: %s\n", user.Name)
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	username := cmd.Args[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      username,
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to create a user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+	fmt.Println("User created successfully:")
+	printUser(user)
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:       %v\n", user.ID)
+	fmt.Printf(" * Name:     %v\n", user.Name)
 }
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/DanilShapilov/gator/internal/config"
+	"github.com/DanilShapilov/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,7 +21,15 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error connecting to db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 	cmds := commands{
@@ -25,16 +37,16 @@ func main() {
 	}
 
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
-	args := os.Args
-	if len(args) < 2 {
+	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
 		return
 	}
 
 	cmd := command{
-		Name: args[1],
-		Args: args[2:],
+		Name: os.Args[1],
+		Args: os.Args[2:],
 	}
 
 	err = cmds.run(&programState, cmd)
